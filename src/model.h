77d3dab736325ec882e94caaad52adf771b99f9f
jniyonkuru <jiniyonkurujimmy@gmail.com>
2024-02-04 17:29:49 +0100

    User input integration for specifying start point and end point

--- src/model.h
+++ src/model.h
@@ -8,27 +8,32 @@
 class Model
 {
 public:
-    struct Node {
+    struct Node 
+    {
         double x = 0.f;
         double y = 0.f;
     };
     
-    struct Way {
+    struct Way 
+    {
         std::vector<int> nodes;
     };
     
-    struct Road {
+    struct Road 
+    {
         enum Type { Invalid, Unclassified, Service, Residential,
             Tertiary, Secondary, Primary, Trunk, Motorway, Footway };
         int way;
         Type type;
     };
     
-    struct Railway {
+    struct Railway 
+    {
         int way;
     };    
     
-    struct Multipolygon {
+    struct Multipolygon 
+    {
         std::vector<int> outer;
         std::vector<int> inner;
     };
@@ -46,16 +51,43 @@ public:
     
     Model( const std::vector<std::byte> &xml );
     
-    auto MetricScale() const noexcept { return m_MetricScale; }    
+    auto MetricScale() const noexcept 
+    { 
+        return m_MetricScale; 
+    }    
     
-    auto &Nodes() const noexcept { return m_Nodes; }
-    auto &Ways() const noexcept { return m_Ways; }
-    auto &Roads() const noexcept { return m_Roads; }
-    auto &Buildings() const noexcept { return m_Buildings; }
-    auto &Leisures() const noexcept { return m_Leisures; }
-    auto &Waters() const noexcept { return m_Waters; }
-    auto &Landuses() const noexcept { return m_Landuses; }
-    auto &Railways() const noexcept { return m_Railways; }
+    auto &Nodes() const noexcept 
+    { 
+        return m_Nodes; 
+    }
+    auto &Ways() const noexcept 
+    { 
+        return m_Ways; 
+    }
+    auto &Roads() const noexcept 
+    { 
+        return m_Roads; 
+    }
+    auto &Buildings() const noexcept
+     { 
+        return m_Buildings;
+     }
+    auto &Leisures() const noexcept 
+    { 
+        return m_Leisures; 
+    }
+    auto &Waters() const noexcept 
+    { 
+        return m_Waters; 
+    }
+    auto &Landuses() const noexcept 
+    { 
+        return m_Landuses;
+    }
+    auto &Railways() const noexcept 
+    { 
+        return m_Railways; 
+    }
     
 private:
     void AdjustCoordinates();
